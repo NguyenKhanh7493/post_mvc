ff35afdf2f5eccfbd11499894415df9b4e0aadab
nguyen khanh <nguyenkhanh7493@gmail.com>
2018-07-11 16:31:05 +0700

    khanh

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $action = $_GET['action'];
-    $controller = $_GET['controller'];
-    $controllerName = $controller . 'Controller';
-    require_once('controllers/'.$controllerName.'.php');
-    //tạo 1 đối tượng
-$controller = new $controllerName();
-$controller->$action();
