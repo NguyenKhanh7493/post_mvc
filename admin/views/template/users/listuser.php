24341355dc269ffb73a9bb400eba4e5b771298b0
nguyen khanh <nguyenkhanh7493@gmail.com>
2018-07-26 16:58:11 +0700

    cuoicung

--- admin/views/template/users/listuser.php
+++ admin/views/template/users/listuser.php
@@ -13,8 +13,8 @@
                 <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                     <h4 class="page-title">Danh sách quản trị viên</h4>
                 </div>
-                <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                    <h4 class="page-title">Danh sách quản trị viên</h4>
+                <div class="col-lg-3 col-md-8 col-sm-8 col-xs-12" style="float: right;font-size: 17px;">
+                    <a href="#"><i class="fa fa-plus" aria-hidden="true"></i> Thêm quản trị viên</a>
                 </div>
                 <!--                <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">-->
                 <!--                    <a href="https://themeforest.net/item/elite-admin-responsive-dashboard-web-app-kit-/16750820" target="_blank" class="btn btn-danger pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Buy Now</a>-->
