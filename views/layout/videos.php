1b2fb760f2a83ccb703961bc0d21dfed4a24ee9f
nguyen khanh <nguyenkhanh7493@gmail.com>
2018-10-15 16:58:48 +0700

    acd

--- views/layout/videos.php
+++ views/layout/videos.php
@@ -1,13 +1,12 @@
 <section class="section mb-0">
     <div class="row">
-
         <!-- World -->
         <div class="col-md-12 mb-30">
             <div class="title-wrap bottom-line bottom-line--orange">
                 <img src="<?php base_url?>/admin/public/upload/bd.png" alt="" style="width: 70px;height: 45px;float: left;margin-right: 8px;">
                 <h3 class="section-title section-title--sm" style="border-bottom: double red;margin-top: 8px;">Bóng đá & cuộc sống</h3>
             </div>
-            <article class="entry" style="height: 350px;border: 1px solid red;padding: 4px;">
+            <article class="entry" style="height: 350px;border: double 4px red;border-radius: 3px;">
                 <div class="entry__img-holder">
                     <a href="single-post.html">
                         <div class="thumb-container thumb-75">
@@ -15,79 +14,9 @@
                         </div>
                     </a>
                 </div>
-
-                <div class="entry__body">
-                    <div class="entry__header">
-                        <h2 class="entry__title">
-                            <a href="single-post.html">To Succeed in 2018, Ecommerce Entrepreneurs Must Focus on This One Change</a>
-                        </h2>
-                        <ul class="entry__meta">
-                            <li class="entry__meta-author">
-                                <i class="ui-author"></i>
-                                <a href="#">DeoThemes</a>
-                            </li>
-                            <li class="entry__meta-date">
-                                <i class="ui-date"></i>
-                                21 October, 2017
-                            </li>
-                            <li class="entry__meta-comments">
-                                <i class="ui-comments"></i>
-                                <a href="#">115</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="entry__excerpt">
-                        <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos. Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                    </div>
-                </div>
             </article>
-            <ul class="post-list-small post-list-small--border-top">
-                <li class="post-list-small__item">
-                    <article class="post-list-small__entry">
-                        <div class="post-list-small__body">
-                            <h3 class="post-list-small__entry-title">
-                                <a href="single-post.html">What Indie Beauty Companies Can Teach Entrepreneurs About Scaling</a>
-                            </h3>
-                            <ul class="entry__meta">
-                                <li class="entry__meta-date">
-                                    <i class="ui-date"></i>
-                                    21 October, 2017
-                                </li>
-                            </ul>
-                        </div>
-                    </article>
-                </li>
-                <li class="post-list-small__item">
-                    <article class="post-list-small__entry">
-                        <div class="post-list-small__body">
-                            <h3 class="post-list-small__entry-title">
-                                <a href="single-post.html">Bill Gates's 5 Favorite Books of 2017</a>
-                            </h3>
-                            <ul class="entry__meta">
-                                <li class="entry__meta-date">
-                                    <i class="ui-date"></i>
-                                    21 October, 2017
-                                </li>
-                            </ul>
-                        </div>
-                    </article>
-                </li>
-                <li class="post-list-small__item">
-                    <article class="post-list-small__entry">
-                        <div class="post-list-small__body">
-                            <h3 class="post-list-small__entry-title">
-                                <a href="single-post.html">Sheryl Sandberg's 6 Steps to Make Sure Everyone Feels Safe at Work</a>
-                            </h3>
-                            <ul class="entry__meta">
-                                <li class="entry__meta-date">
-                                    <i class="ui-date"></i>
-                                    21 October, 2017
-                                </li>
-                            </ul>
-                        </div>
-                    </article>
-                </li>
-            </ul>
+            <!--            test nè-->
+            <!--            test nè-->
         </div> <!-- end world -->
 
         <!-- Science -->
